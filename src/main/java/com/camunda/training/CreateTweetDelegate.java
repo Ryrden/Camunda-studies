c100fb0cd7ab2c2339e252e9bbb1b1b9514554da
Ryan S <ryansateles@gmail.com>
2022-12-26 13:06:09 -0300

    Exercice 3b - Usin JavaDelegate

---
+++ src/main/java/com/camunda/training/CreateTweetDelegate.java
@@ -0,0 +1,24 @@
+package com.camunda.training;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import twitter4j.Twitter;
+import twitter4j.TwitterFactory;
+import twitter4j.auth.AccessToken;
+
+public class CreateTweetDelegate implements JavaDelegate {
+    private final Logger LOGGER = LoggerFactory.getLogger(CreateTweetDelegate.class.getName());
+
+    public void execute(DelegateExecution execution) throws Exception {
+        String content = "I did it! Cheers Ryan Souza";
+        LOGGER.info("Publishing tweet: " + content);
+        AccessToken accessToken = new AccessToken("220324559-YfhutOgjLKsgPFXGGkda4q72rfeEp2nprXZHDTQf", "QmlH4CUdDDav3u2rrE2MQCmWwKfVHkcc8qlGpu6SMn0uD");
+        Twitter twitter = new TwitterFactory().getInstance();
+        twitter.setOAuthConsumer("XSQGUIOwiOs8p55NMijlObpPu", "ZXDVbURd1WMUFBmygQ4Je8PPR0gkaKwczkgQ6YfgPfF0MYnIzY");
+        twitter.setOAuthAccessToken(accessToken);
+        twitter.updateStatus(content);
+    }
+}
